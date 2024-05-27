a54a4f7aaf3359ba7fb3fa8cb911c2a37fd0e50f
AarushLaddha <147957285+AarushLaddha@users.noreply.github.com>
2024-05-27 11:21:06 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+javascriptCopy code
+// Responsive navigation
+const navToggle = document.querySelector('header nav');
+const navList = document.querySelector('header nav ul');
+navToggle.addEventListener('click', () => {
+  navList.classList.toggle('show');
+});
